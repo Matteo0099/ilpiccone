4faeb70278875a3e3eb039a604a5bf685a99e3c7
Matty <matteomania05@gmail.com>
2022-05-14 12:10:11 +0200

    Modifiche ai "comandi 3IA", resize e sfondi/palette
    Responsive layout da 90%<X<125%  sfondi, palette colori, resize testo/immagini, css e js per le tabelle.

--- jvscpt-1.js
+++ jvscpt-1.js
@@ -1,16 +1,16 @@
 var app = document.getElementById('app');
 
 var typewriter = new Typewriter(app, {
-    delay: 82,
+    delay: 84,
 });
       
   typewriter
     .pauseFor(100)
-    .pauseFor(120)
-    .typeString("Benvenuto Nel Sito \"Del Piccone\"")
     .pauseFor(700)
-    .typeString("\n")
-    .deleteChars(33)
-    .pauseFor(700)
-    .typeString("Scorri in basso per visualizzare Il PowerPoint!⛏️")
+    //.deleteChars(33)
+    //.pauseFor(700)
+    .typeString("\nScorri in basso per visualizzare Il PowerPoint!⛏️")
     .start();
+
+
+    
